a348e6f0499cca73177a047a8e0e932b8d83fccd
tayavanina <144583644+tayavanina@users.noreply.github.com>
2026-09-12 16:05:42 +0000

    Deploying to gh-pages from @ tayavanina/goit-js-hw-10@ae38c7c524cc9d866cbe8f80888a7852c9be67bd 🚀

---
+++ assets/2-snackbar-BYxqaViR.js
@@ -0,0 +1,2 @@
+import{i as o}from"./vendor-BbSUbo7J.js";const r=document.querySelector(".form");r.addEventListener("submit",s=>{s.preventDefault();const t=Number(s.target.delay.value),i=s.target.state.value;new Promise((e,m)=>{setTimeout(()=>{i==="fulfilled"?e(`✅ Fulfilled promise in ${t}ms`):m(`❌ Rejected promise in ${t}ms`)},t)}).then(e=>{o.show({message:e})}).catch(e=>{o.show({message:e})})});
+//# sourceMappingURL=2-snackbar-BYxqaViR.js.map
